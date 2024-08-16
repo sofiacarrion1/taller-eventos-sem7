995d7941ddc2f183f8f4b70cf1850061778bdcfb
sofiacarrion1 <sofiacarrion2815@gmail.com>
2024-08-16 19:52:17 -0300

    solucion click agregada

--- eventos.js
+++ eventos.js
@@ -3,3 +3,16 @@ document.getElementById('container').onclick = function(){
     alert('Hola! Soy el div');
 }
 // Nueva solucion para evitar que no se disparen ambos eventos a la vez:
+document.addEventListener('DOMContentLoaded' , function() {
+    let div = document.getElementById(`container`);
+    let button = document.getElementById(`btn`);
+
+    button.addEventListener(`click`, function(event){
+        alert(`Hola!`)
+        event.stopPropagation();
+div.addEventListener('click', function (){
+    alert ('Hola! soy el div');
+});
+
+    });
+});
